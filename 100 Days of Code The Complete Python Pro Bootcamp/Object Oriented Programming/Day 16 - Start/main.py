ede3e6430b96cd7146e6cc38b48f6668b2348803
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-09-03 00:27:48 -0400

    Change Turtle Color

--- 100 Days of Code The Complete Python Pro Bootcamp/Object Oriented Programming/Day 16 - Start/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/Object Oriented Programming/Day 16 - Start/main.py
@@ -1,3 +1,11 @@
+"""
+Turtle Graphics Documentation
+https://docs.python.org/3/library/turtle.html
+
+Turtle Colours
+https://cs111.wellesley.edu/reference/colors
+"""
+
 import another_module
 print(another_module.another_variable)
 
@@ -5,6 +13,8 @@ from turtle import Turtle, Screen
 timmy = Turtle()
 print(timmy)
 timmy.shape("turtle")
+timmy.color("DarkRed")
+timmy.forward(100)
 
 my_screen = Screen()
 print(my_screen.canvheight)
