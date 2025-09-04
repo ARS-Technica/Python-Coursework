3c5323098dd73b3daaa2c0ac09f10d71fb2f6e7a
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-09-03 23:27:40 -0400

    Creating a Table using PrettyTable library

--- 100 Days of Code The Complete Python Pro Bootcamp/Object Oriented Programming/Day 16 - Start/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/Object Oriented Programming/Day 16 - Start/main.py
@@ -4,18 +4,37 @@ https://docs.python.org/3/library/turtle.html
 
 Turtle Colours
 https://cs111.wellesley.edu/reference/colors
+
+PrettyTable
+https://pypi.org/project/prettytable/
+
+PrettyTable Tutorial
+https://code.google.com/archive/p/prettytable/wikis/Tutorial.wiki
 """
 
-import another_module
-print(another_module.another_variable)
+# import another_module
+# print(another_module.another_variable)
+#
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkRed")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("DarkRed")
-timmy.forward(100)
+from prettytable import PrettyTable
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmader"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+# print(table.align)
+
+table.align = "l"   # Left align
+print(table)
+
