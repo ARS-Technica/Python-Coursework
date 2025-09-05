a056ae2ba9b2366774530ccab9a8049f1a106529
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-09-04 22:47:31 -0400

    Update start.py

--- 100 Days of Code The Complete Python Pro Bootcamp/Day 17 - The Quiz Project/start.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/Day 17 - The Quiz Project/start.py
@@ -15,3 +15,16 @@ user_2.username = "Jack"
 print(user_2.username)  # Returns "Jack"
 
 
+#%%
+
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+
+user_1 = User("001", "John")
+print(user_1.username)  # Returns "John"
+
+user_2 = User()     # TypeError: __init__() missing 2 required positional arguments: 'user_id' and 'username'
+user_2.id = "002"
+user_2.username = "Jack"
