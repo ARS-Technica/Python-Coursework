66c9f22fd2ce117c3c444b67ddd46db7563f7cc2
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-09-04 22:33:54 -0400

    Create start.py

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/Day 17 - The Quiz Project/start.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self):
+        print("New user being created...")
+
+user_1 = User()
+user_1.id = "001"
+user_1.username = "John"
+
+print(user_1.username)  # Returns "John"
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "Jack"
+
+print(user_2.username)  # Returns "Jack"
+
+
