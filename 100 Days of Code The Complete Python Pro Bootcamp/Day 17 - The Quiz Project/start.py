f215fff0d12372b978445071d44979150fb0adba
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-09-04 23:35:56 -0400

    Update start.py

--- 100 Days of Code The Complete Python Pro Bootcamp/Day 17 - The Quiz Project/start.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/Day 17 - The Quiz Project/start.py
@@ -44,3 +44,26 @@ user_2 = User("002", "Jack")
 
 print(user_1.followers)  # Returns 0 (Default value)
 
+
+#%%
+
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0  # Default value
+        self.following = 0  # Default value
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+user_1 = User("001", "John")
+user_2 = User("002", "Jack")
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
