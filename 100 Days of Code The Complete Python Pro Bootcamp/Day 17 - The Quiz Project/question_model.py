308b4e09b78c0dc06baffe03b8fdb507622a8bcc
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-09-17 21:21:04 -0400

    Created Question Class

--- 100 Days of Code The Complete Python Pro Bootcamp/Day 17 - The Quiz Project/question_model.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/Day 17 - The Quiz Project/question_model.py
@@ -1,1 +1,7 @@
+class Question:
+    def __init__(self, question_text, answer_text):
+        self.question_text = question_text
+        self.answer_text = answer_text
 
+new_question = Question("Something here", "False")
+print(new_question.question_text)
